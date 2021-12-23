b60d0b9a8cbae8e0383eb6f87e1cc86442064c10
Jinsil Han <truth.h406@gmail.com>
2021-12-23 18:20:35 +0900

    ✨ 환경설정, 과제1, 과제2

---
+++ src/pages/wines.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+
+const Wines: NextPage = () => {
+    return(
+        <div>
+            <h1>Wine</h1>
+        </div>
+    )
+}
+
+export default Wines;
+//Typescript를 쓰는 가장 기본적인 템플릿.
